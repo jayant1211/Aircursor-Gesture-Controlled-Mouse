f4dcd8f3d3f5dd9b696dad88b9ecabf0ceb888d4
Jayant Meshram <jayant.meshram18@vit.edu>
2021-08-25 14:52:28 +0530

    Initial Commit

--- HandDetctionModule.py
+++ HandDetctionModule.py
@@ -1,6 +1,21 @@
-##
+'''
+*********APPROACH*********
 #Step1
-###Detcet hand--->yes--->step2
-###-------------->no---->continue
+Detect hand--->yes--->step2
+-------------->no---->continue
 
 #Step2
+Case I: Single tip
+    Implement Mouse Pointer Movement--->movement()
+
+Case II: Joint
+    SubCase 1: Twice tap in Short Duration
+        Implement Left Click----------->leftClick()
+    SubCase 2: Hold for more than 2 Sec
+        Implement Right Click---------->rightClick()
+    SubCase 3: Hold and move
+        Implement Drag if applicable--->drag()
+
+Case III: Anything else
+    Continue to next Frame
+'''
