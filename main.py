fc40fffb65e9fdbae2dae08bd9e3cba9079110fb
Jayant Meshram <jayant.meshram18@vit.edu>
2021-08-25 17:13:47 +0530

    added locations for each landmark

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+
 while True:
     _, frame = cap.read()
     frame = hdm.detectHand(frame)
+    #x,y,z = hdm.detectCase()
+    #print("x : ",x,"y : ",y,"z : ",z)
+    #hdm.detectCase(locs)
     cv2.imshow("hello",frame)
     if cv2.waitKey(5) & 0xFF == 27:
         break
+
 cap.release()
